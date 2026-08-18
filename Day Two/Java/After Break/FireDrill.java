69081b739fd5fff3d0f5c8228c5ea1c34e112daf
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-08-18 17:51:01 +0100

    Added new files for Java Firedrill files

---
+++ Day Two/Java/After Break/FireDrill.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+
+public class FireDrill {
+
+public static int fatherSonAge(current_father_age current_son_age){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter father's age");
+    int userInput1 = scanner.nextInt();
+
+    System.out.println("Enter father's age");
+     int userInput2 = scanner.nextInt();
+
+    int current_father_age = userInput1;
+    int current_son_age = userInput2;
+
+    if(current_father_age > current_son_age);
+    int age_differences = (current_father_age / current_son_age);
+
+    int father_age_years_ago = current_father_age - current_son_age * 2;
+    return current_father_age, current_son_age; 
+
+public staic void main(String[] args) {
+System.out.println(father_age_years_ago);
+
+}
+
+
+}
+
+
+}
+
---
+++ Day Two/Java/After Break/FireDrill2.java
@@ -0,0 +1,20 @@
+
+#complete the  function so that it finds the average of the three scores passed to it and returns the letter value #associated with that grade.
+
+public class FireDrill2
+public static int numericalScore(score);
+    return score
+    int score = 80;
+    int studentNumber = 5;
+        if(90 <= score <= 100);
+           System.out.print(""
+        if(80 <= score < 90);
+            print("B")
+        if(70 <= score < 80);
+            print("C")
+        if 60 <= score < 70:
+            print('D')
+        if 0 <= score < 60:
+            print('F') 
+int average = score / 
+print(average)
