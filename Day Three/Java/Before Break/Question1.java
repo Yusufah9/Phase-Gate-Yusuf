90ce8abee1007a633159247507cb8bf88b67072a
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-08-19 14:01:37 +0100

    Added few things

---
+++ Day Three/Java/Before Break/Question1.java
@@ -0,0 +1,72 @@
+// reverse a given string using a for-loop
+
+
+
+public class Question1 {
+    public static void main(String[] args) {
+    String name = "Yusuf";
+    for(int count = 0; count < name.length - 1; count--);
+    if(name.length == name);
+    System.out.println(name);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char1 = input("Enter first character: ")[0]
+char2 = input("Enter second character: ")[0]
+
+if char1 < char2:
+    print(f"Smallest char: {char1}, Largest char: {char2}")
+else:
+print(f"Smallest char: {char2}, Largest char: {char1}")
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Yusuf";
+        char letter = name.charAt(1);
+        System.out.println(letter);
+    }
+}
+
