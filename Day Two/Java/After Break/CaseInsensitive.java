dd728c11060502a25408b639d784dfe06358798c
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-08-19 17:12:17 +0100

    Aded few things

---
+++ Day Two/Java/After Break/CaseInsensitive.java
@@ -0,0 +1,12 @@
+
+public class CaseInsensitive {
+    public static String caseInsensitive(String text) {
+        String result = text.toLowerCase();
+            } return text
+
+    public static void main(String[] args) {
+        System.out.println(countDuplicates("abcdef"));  
+        System.out.println(countDuplicates("a111BAcb")); 
+    }
+}
+
---
+++ Day Two/Java/After Break/lowercase.py
@@ -0,0 +1,10 @@
+def count_repeating_characters(text1, text2):
+    text = text.lower()
+    return text
+    for char in text:
+        
+            
+    return repeating_count
+print(text("abcdef"))  
+print(text2("a111BAcb"))
+
